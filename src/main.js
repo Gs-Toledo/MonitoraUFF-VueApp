e42be213d434455eddaf134bb6ddb5e17ec9a1ef
Giovanni Toledo <giovanni.toledo@injunior.com.br>
2024-09-27 20:23:16 -0300

    feat: adicionando tailwind e vuetify

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'  // Importe a store do Vuex
 
+import vuetify from './plugins/vuetify'
+import '@/assets/styles.css'
+
 
 const app = createApp(App)
 
 
 app.use(store)
+app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,18 +1,18 @@
 <template>
-  <main>
-    <div>
-      <div>
-        <label for="usernameInput">Username</label>
-        <input type="text" id="usernameInput" v-model="username" />
+<main class="flex justify-center items-center min-h-screen bg-black">
+    <div class="border-2 border-green-500 p-6 rounded-lg bg-gray-800">
+      <div class="mb-4">
+        <label for="usernameInput" class="block text-white">Username</label>
+        <input type="text" id="usernameInput" v-model="username" class="border p-2 w-full bg-gray-700 text-white" />
       </div>
 
-      <div>
-        <label for="passwordInput">Password</label>
-        <input type="password" id="passwordInput" v-model="password" />
+      <div class="mb-4">
+        <label for="passwordInput" class="block text-white">Password</label>
+        <input type="password" id="passwordInput" v-model="password" class="border p-2 w-full bg-gray-700 text-white" />
       </div>
 
-      <button type="button" id="button-login" @click="executeLogin">Enviar</button>
-      <div v-if="errorMessage">{{ errorMessage }}</div>
+      <button type="button" id="button-login" @click="executeLogin" class="w-full bg-green-500 text-white p-2 rounded hover:bg-green-600">Enviar</button>
+      <div v-if="errorMessage" class="mt-4 text-red-500">{{ errorMessage }}</div>
     </div>
   </main>
 </template>
