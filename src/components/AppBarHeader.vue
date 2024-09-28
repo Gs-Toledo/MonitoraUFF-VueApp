3e64353d3c4a4241e8f09fe823d82c700d6406df
Giovanni Toledo <giovanni.toledo@injunior.com.br>
2024-09-28 18:40:46 -0300

    feat: logout funcional

--- src/components/AppBarHeader.vue
+++ src/components/AppBarHeader.vue
@@ -1,25 +1,31 @@
 <template>
-<v-app-bar :elevation="2">
-      <template v-slot:prepend>
-        <v-app-bar-nav-icon>
-          <v-icon icon="mdi-menu"></v-icon>
-        </v-app-bar-nav-icon>
-      </template>
-      <v-app-bar-title>Monitora Uff</v-app-bar-title>
-      <template v-slot:append>
-        <v-btn icon="mdi-dots-vertical"></v-btn>
-        <v-btn icon="mdi-logout" @click="userlogout"></v-btn>
-      </template>
-    </v-app-bar>
+  <v-app-bar :elevation="2">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon>
+        <v-icon icon="mdi-menu"></v-icon>
+      </v-app-bar-nav-icon>
+    </template>
+    <v-app-bar-title>Monitora Uff</v-app-bar-title>
+    <template v-slot:append>
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+      <v-btn icon="mdi-logout" @click="userlogout"></v-btn>
+    </template>
+  </v-app-bar>
 </template>
 
-
 <script>
+import router from '@/router'
+import { mapActions } from 'vuex'
+
 export default {
-    methods: {
-        userlogout() {
-            console.log('deslogando...')
-        }
+
+  methods: {
+    ...mapActions(['logout']),
+    userlogout() {
+      console.log('deslogando...')
+      this.logout(router)
+      
     }
+  }
 }
 </script>
