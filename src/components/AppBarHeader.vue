169fb6729b1389c0de589ca762135ab502235069
Giovanni Toledo <giovanni.toledo@injunior.com.br>
2024-09-28 18:46:14 -0300

    feat: modal logout

--- src/components/AppBarHeader.vue
+++ src/components/AppBarHeader.vue
@@ -8,9 +8,21 @@
     <v-app-bar-title>Monitora Uff</v-app-bar-title>
     <template v-slot:append>
       <v-btn icon="mdi-dots-vertical"></v-btn>
-      <v-btn icon="mdi-logout" @click="userlogout"></v-btn>
+      <v-btn icon="mdi-logout" @click="showLogoutModal = true"></v-btn>
     </template>
   </v-app-bar>
+
+  <v-dialog v-model="showLogoutModal" max-width="400">
+    <v-card>
+      <v-card-title class="headline">Confirmar Logout</v-card-title>
+      <v-card-text>Você tem certeza que deseja sair?</v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn text @click="showLogoutModal = false">Cancelar</v-btn>
+        <v-btn color="blue darken-1" text @click="userlogout">Confirmar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
@@ -18,13 +30,17 @@ import router from '@/router'
 import { mapActions } from 'vuex'
 
 export default {
-
+  data() {
+    return {
+      showLogoutModal: false
+    }
+  },
   methods: {
     ...mapActions(['logout']),
     userlogout() {
       console.log('deslogando...')
       this.logout(router)
-      
+      this.showLogoutModal = false
     }
   }
 }
