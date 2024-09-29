a4b97aa0ea863554faef5dccc89ed40b589a95ad
Giovanni Toledo <giovanni.toledo@injunior.com.br>
2024-09-29 16:59:26 -0300

    style: page login with vuetify

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,41 +1,65 @@
 <template>
-  <main class="flex justify-center items-center min-h-screen bg-black">
-    <div class="border-2 border-green-500 p-6 rounded-lg bg-gray-800">
-      <div class="mb-4">
-        <label for="usernameInput" class="block text-white">Username</label>
-        <input
-          type="text"
-          id="usernameInput"
-          v-model="username"
-          class="border p-2 w-full bg-gray-700 text-white"
-        />
+  <div>
+    <v-img
+      class="mx-auto my-6"
+      max-width="228"
+      src="https://upload.wikimedia.org/wikipedia/pt/4/47/UFF_bras%C3%A3o.png"
+    ></v-img>
+
+    <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
+      <div class="text-subtitle-1 text-medium-emphasis">Login</div>
+
+      <v-text-field
+        density="compact"
+        placeholder="Insira o Login"
+        prepend-inner-icon="mdi-email-outline"
+        v-model="username"
+        variant="outlined"
+      ></v-text-field>
+
+      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+        Senha
+
+        <!--    <a class="text-caption text-decoration-none text-blue" href="#" rel="noopener noreferrer" target="_blank">
+          Esqueceu a senha?</a> -->
       </div>
 
-      <div class="mb-4">
-        <label for="passwordInput" class="block text-white">Password</label>
-        <input
-          type="password"
-          id="passwordInput"
-          v-model="password"
-          class="border p-2 w-full bg-gray-700 text-white"
-        />
-      </div>
+      <v-text-field
+        :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="isPasswordVisible ? 'text' : 'password'"
+        density="compact"
+        placeholder="Insira a senha"
+        prepend-inner-icon="mdi-lock-outline"
+        variant="outlined"
+        @click:append-inner="isPasswordVisible = !isPasswordVisible"
+        v-model="password"
+      ></v-text-field>
 
-      <button
-        type="button"
-        id="button-login"
-        @click="executeLogin"
+      <v-btn
+        block
+        class="mb-8"
+        color="blue"
+        size="large"
+        variant="tonal"
         :disabled="isSendingRequest"
-        :class="{
-          'w-full bg-green-500 text-white p-2 rounded hover:bg-green-600': true,
-          'opacity-50 cursor-not-allowed': isSendingRequest
-        }"
+        @click="executeLogin"
       >
-        Enviar
-      </button>
+        Entrar
+      </v-btn>
+
+      <!-- <v-card-text class="text-center">
+        <a
+          class="text-blue text-decoration-none"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Cadastre-se <v-icon icon="mdi-chevron-right"></v-icon>
+        </a>
+      </v-card-text> -->
       <div v-if="errorMessage" class="mt-4 text-red-500">{{ errorMessage }}</div>
-    </div>
-  </main>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -47,7 +71,8 @@ export default {
       username: '',
       password: '',
       errorMessage: '',
-      isSendingRequest: false
+      isSendingRequest: false,
+      isPasswordVisible: false
     }
   },
   methods: {
